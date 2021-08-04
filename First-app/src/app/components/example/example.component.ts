22e3139f9235ca6e23be5aa4dae5bd2126c56cbc
Miguel Martinez Diaz <mmd835@alumnos.unican.es>
2021-08-04 20:36:04 +0200

    Primera subida de los elementos de la capa de Presentación de Polaflix

---
+++ First-app/src/app/components/example/example.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+  selector: 'example',
+  template: `
+  <h1>Hola mundo, soy MI COMPONENTE</h1>
+  <p>Este es mi primer componente</p>
+  `
+})
+export class ExampleComponent{}
