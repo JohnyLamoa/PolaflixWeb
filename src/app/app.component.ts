3df84318bdfcbd2d60cb9c720ed7e168d128cfab
Miguel Martinez Diaz <mmd835@alumnos.unican.es>
2021-09-09 23:42:37 +0200

    Subida del Jueves 9 de Septiembre, corregido heder tabla facturación y nombres de las Series

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First-app';
+  title = 'First-app hue hue hue';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Router, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+/**
+* Material Modules
+*/
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table'
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponents,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatListModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
